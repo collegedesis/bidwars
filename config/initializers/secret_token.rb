90233f24bf86c9975b2d5a0f6c38b1d51818b7a8
Mehul Kar <mehul.kar@gmail.com>
2013-02-02 16:02:39 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidwars::Application.config.secret_token = '3e2659375390381ef6083f90e4c73e354cc0633672b613deacb2aa544bbefbf45284c97fe40a30b6004db85bfd131d9147cdc7758d812f096e26f66f0956fed1'
