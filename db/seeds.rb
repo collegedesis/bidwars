c69bb05064248dab0d9f5b7f9ab61468c52390d0
Mehul Kar <mehul.kar@gmail.com>
2013-02-03 03:19:11 -0800

    add links to djs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,210 @@
 djs = Dj.create([
-  { stage_name: 'Klasikhz', full_name: 'Iqwak Bhuji', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Sanjoy Deb', full_name: 'Sanjoy Deb', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: '2 Nyce', full_name: 'Neet Mangat', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Swizzee', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ KA$H', full_name: 'Karan Thakkar', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Dr. Srimix', full_name: 'Srihari Sritharan', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Sleepy Z Beats', full_name: 'Naeim Raza', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Ravi B', full_name: 'Ravi Bhavsar', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Mr.Shaw', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'G-tam', full_name: 'Gautam Murali', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Palladium', full_name: 'Anant Patel', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Xtasis', full_name: 'Pranav Misra', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Nimz', full_name: 'Nirmal Pathak', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJLogz', full_name: 'Rishabh Alaap Singh', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Mahogany', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Deep Deb', full_name: 'Deep Deb', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Havik', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Ripudaman Joshi', full_name: 'Ripudaman Joshi', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Sansa', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Shaun Atwal', full_name: 'Shaun Atwal', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ MB / Delta Zero', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Aakamazing', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Dj MadHat', full_name: '', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Shyam Murali', full_name: 'Shyam Murali', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'proDiJy', full_name: 'Poojan Bhungalia', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DjK', full_name: 'Kaushal Patel', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Milan Goswami', full_name: 'Milan Goswami', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Arvy', full_name: 'Aravind Sriram', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Vissagan', full_name: 'Vissagan Gopalakrishnan', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ MoMoney', full_name: 'Mohit Chhatpar', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'DJ Ra', full_name: 'Rahul Kataria', twitter: '', facebook: '', soundcloud: '' },
-  { stage_name: 'Pranesh', full_name: 'Pranesh Sai Vadlamudi', twitter: '', facebook: '', soundcloud: '' },
-  ])
+  {
+    soundcloud: 'klasikhzmusic', 
+    facebook: 'pages/Klasikhz/235721835972',
+    twitter: 'klasikhz',
+    stage_name: 'Klasikhz',
+    full_name: 'Iqwak Bhuji'
+  },
+  {
+    soundcloud: 'sanjoy', 
+    facebook: 'DjSanjoy',
+    twitter: 'sanjoyd',
+    stage_name: 'Sanjoy Deb',
+    full_name: 'Sanjoy Deb'
+  },
+  {
+    soundcloud: '2nyce', 
+    facebook: 'TheOfficial2NyCe',
+    twitter: 'AffinitySounds',
+    stage_name: '2 Nyce',
+    full_name: 'Neet Mangat'
+  },
+  {
+    soundcloud: 'swizzee', 
+    facebook: 'swizzee',
+    twitter: 'swizzee',
+    stage_name: 'Swizzee',
+    full_name: 'Swi'
+  },
+  {
+    facebook: 'djkashonline', 
+    twitter: 'djkashonline',
+    stage_name: 'DJ KA$H',
+    full_name: 'Karan Thakkar'
+},
+  {
+    soundcloud: 'dj-srimix', 
+    facebook: 'DrSrimix',
+    twitter: 'DrSrimix',
+    stage_name: 'Dr. Srimix',
+    full_name: 'Srihari Sritharan'
+  },
+  {
+    soundcloud: 'sleepyz', 
+    facebook: 'SleepyZMusic',
+    twitter: 'sleepyzbeats',
+    stage_name: 'Sleepy Z Beats',
+    full_name: 'Naeim Raza'
+  },
+  {
+    soundcloud: 'djravibonline', 
+    facebook: 'djravib',
+    twitter: 'djravib',
+    stage_name: 'Ravi B',
+    full_name: 'Ravi Bhavsar'
+  },
+  {
+    twitter: 'mistershaw1',
+    stage_name: 'Mr.Shaw',
+    full_name: 'Sahaj Shah'
+  },
+  {
+    soundcloud: 'g-tam', 
+    facebook: 'DJGtam',
+    twitter: 'itsgtam',
+    stage_name: 'G-tam',
+    full_name: 'Gautam Murali'
+  },
+  {
+    soundcloud: 'PalladiumMusic', 
+    facebook: 'PalladiumMusic',
+    twitter: 'Palladiumusic',
+    stage_name: 'Palladium',
+    full_name: 'Anant Patel'
+  },
+  {
+    soundcloud: 'xtasis', 
+    facebook: 'XtasisTracks',
+    twitter: 'XtasisTracks',
+    stage_name: 'Xtasis',
+    full_name: 'Pranav Misra'
+  },
+  {
+    soundcloud: 'nirmalpathak', 
+    twitter: 'dirtydragonnimz',
+    stage_name: 'DJ Nimz',
+    full_name: 'Nirmal Pathak'
+  },
+  {
+    soundcloud: 'rishabhalaapsingh', 
+    facebook: 'rishabhandpranesh',
+    twitter: 'djlogz',
+    stage_name: 'DJLogz',
+    full_name: 'Rishabh Alaap Singh'
+  },
+  {
+    soundcloud: 'dj-499', 
+    facebook: 'parth',
+    twitter: 'DJMahoganyy',
+    stage_name: 'DJ Mahogany',
+    full_name: 'Parth Moradia'
+  },
+  {
+    facebook: 'DeepDeb',
+    twitter: 'RollinD33P',
+    stage_name: 'Deep Deb',
+    full_name: 'Deep Deb',
+    soundcloud: 'Notorious', 
+  },
+  {
+    twitter: 'djhavik',
+    stage_name: 'DJ Havik',
+    full_name: 'Nishant Patel',
+    soundcloud: 'sivik',
+    facebook: 'nishantpatel21',
+  },
+  {
+    soundcloud: '/', 
+    twitter: 'ripj',
+    stage_name: 'Ripudaman Joshi',
+    full_name: 'Ripudaman Joshi',
+    soundcloud: 'ripj', 
+    facebook: 'ripudaman',
+  },
+  {
+    twitter: 'djsansa1',
+    stage_name: 'DJ Sansa',
+    full_name: 'Tanaeem Ahamed',
+    facebook: 'pages/DJ-Sansa/335719086461807',
+    soundcloud: 'dj-sansa'
+  },
+  {
+    facebook: 'shaun', 
+    twitter: 'Shaun_Atwal',
+    stage_name: 'Shaun Atwal',
+    full_name: 'Shaun Atwal'
+  },
+  {
+    soundcloud: 'manu-bhatia', 
+    facebook: 'manu.bhatia',
+    twitter: 'manu_bhatia',
+    stage_name: 'DJ MB / Delta Zero',
+    full_name: 'Manu Bhatia'
+  },
+  {
+    facebook: 'aakash',
+    stage_name: 'DJ Aakamazing',
+    full_name: 'Aakash Saraiya',
+    soundcloud: 'aakamazing'
+  },
+  {
+    soundcloud: 'djmadhat',
+    stage_name: 'Dj MadHat',
+    full_name: 'Manoo Murthy',
+  },
+  {
+    facebook: 'nightmusic8246', 
+    stage_name: 'Shyam Murali',
+    full_name: 'Shyam Murali',
+    soundcloud: 'nightmusic8246', 
+  },
+  {
+    
+    facebook: 'poojan',
+    stage_name: 'proDiJy',
+    full_name: 'Poojan Bhungalia',
+    soundcloud: 'poojan', 
+  },
+  {
+    
+    soundcloud: 'kaushal', 
+    stage_name: 'DjK',
+    full_name: 'Kaushal Patel',
+  },
+  {
+    
+    stage_name: 'Milan Goswami',
+    full_name: 'Milan Goswami',
+    soundcloud: 'milangoswami', 
+  },
+  {
+    
+    stage_name: 'Arvy',
+    full_name: 'Aravind Sriram',
+    soundcloud: 'djarvy', 
+  },
+  {
+    stage_name: 'Vissagan',
+    full_name: 'Vissagan Gopalakrishnan',
+  },
+  {
+    facebook: 'mohitmomoney', 
+    stage_name: 'DJ MoMoney',
+    full_name: 'Mohit Chhatpar',
+    soundcloud: 'djmomoney11', 
+  },
+  {
+    soundcloud: 'theofficialdjra', 
+    facebook: 'theofficialdjra',
+    stage_name: 'DJ Ra',
+    full_name: 'Rahul Kataria',
 
-#  @klasikhz, /pages/Klasikhz/235721835972, 
-#  @sanjoyd, /DjSanjoy, /sanjoy
-#  @AffinitySounds, /TheOfficial2NyCe, /2nyce
-#  @swizzee, /swizzee.music, /swizzee
-#  @djkashonline, /djkashonline, 
-#  @DrSrimix, /DrSrimix, /dj-srimix
-#  @sleepyzbeats, /SleepyZMusic, /sleepyz
-#  @djravib, /djravib, /djravibonline
-#  @mistershaw1 / Mr.Shaw 
-#  @itsgtam, /DJGtam, /g-tam
-#  @Palladiumusic, /PalladiumMusic, /PalladiumMusic
-#  @XtasisTracks, /XtasisTracks, /xtasis-tracks
-#  @dirtydragonnimz, ,/nirmalpathak
-#  @djlogz,/rishabhandpranesh, /rishabhalaapsingh
-#  @DJMahoganyy, /parth.moradia, /DJ Mahagany
-#  @sodhivine, /naanak
-#  @RollinD33P, /DeepDeb, /Notorious D.E.B.
-#  @djhavik /nishantpatel21/ sivik or djhavik21
-#  @ripj, /ripudaman.joshi, /ripj
-#  @djsansa1, DJ Sansa, DJ Sansa
-#  @Shaun_Atwal, /shaun.atwal.1, /shaunatwal
-# @manu_bhatia, /manu.bhatia , /manu-bhatia'
-#  ,/aakash.saraiya.7, /aakamazing
-#  @, , /djmadhat
-#  @, /nightmusic8246, /nightmusic8246
-#  /poojan.bhungalia, /prodijyprodijy
-#  @, , /kaushal-1
-#  @, , /milangoswami
-#  @, , djarvy
-#  ,/vg.p  roductionz, /vissagan
-#  , /mohitmomoney, /djmomoney11
-#  @,/theofficialdjra, /theofficialdjra
-#  , , /praneshs
+  },
+  {
+    stage_name: 'Pranesh',
+    full_name: 'Pranesh Sai Vadlamudi',
+    facebook: 'praneshs',
+    soundcloud: 'praneshs'
+  },
+]) 
