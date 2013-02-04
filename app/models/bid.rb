f7e60469371ae9df848ab5256f03fe5c0d1e4f6d
Mehul Kar <mehul.kar@gmail.com>
2013-02-03 18:08:07 -0800

    tweet when bid has been created

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,4 +4,10 @@ class Bid < ActiveRecord::Base
   belongs_to :competition
   has_many :votes
   validates_presence_of :competition, :dj
+  after_create :send_tweet
+
+  def send_tweet
+    str = "#{competition.name} bid #{offering} on #{dj.twitter_mention} for #TheBestBrownParty."
+    Twitter.update(str)
+  end
 end
--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,4 +1,8 @@
 class Dj < ActiveRecord::Base
   attr_accessible :facebook, :full_name, :soundcloud, :stage_name, :twitter
   has_many :bids
+
+  def twitter_mention
+    twitter? ? "@#{twitter}" : stage_name
+  end
 end
