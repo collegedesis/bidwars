bbddb4e0bcae8cadcb8c2a518eb57a65260344ab
Mehul Kar <mehul.kar@gmail.com>
2013-02-03 23:11:55 -0800

    restyle the djs page

--- app/helpers/djs_helper.rb
+++ app/helpers/djs_helper.rb
@@ -1,2 +1,13 @@
-module DjsHelper
+module DjsHelper  
+  def random_bid
+    @offerings ||= [
+      "500 wild buffalos",
+      "7 mom-made gulab jamuns",
+      "A shoutout",
+      "A thank you card",
+      "Love"
+    ]
+    @offerings.shuffle!
+    @offerings.length ? @offerings.pop : ""
+  end
 end
